56e997d503aa70cd18a0aec609f198c5db16b731
unknown <bistaamosh@gmail.com>
2026-08-14 13:31:34 +0545

    stampynp

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+        origin: ['https://stampynp.netlify.app', 'http://localhost:5173'],
         credentials: true,
     })
 );
